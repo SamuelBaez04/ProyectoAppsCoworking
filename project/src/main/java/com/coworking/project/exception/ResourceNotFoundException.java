118908fb56b9c81221831b08620b2f3a1f6be2c5
NataSabogal <natalia.sabogal.1766@eam.edu.co>
2025-09-27 17:23:01 -0500

    Test commit

---
+++ project/src/main/java/com/coworking/project/exception/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.coworking.project.exception;
+
+/**
+ * Excepción para recursos no encontrados
+ */
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    public ResourceNotFoundException(String resource, String field, Object value) {
+        super(String.format("%s no encontrado con %s: %s", resource, field, value));
+    }
+}
