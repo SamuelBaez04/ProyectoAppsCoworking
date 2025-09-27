1cc648422ae90ca6bbd36ab37e9c23dd306b4474
SamuelBaez04 <samuel.valencia.7088@eam.edu.co>
2025-09-26 21:55:44 -0500

    Repositories actualizados y terminados de crear, fix nombre ReservaRepository

--- project/src/main/java/com/coworking/project/persistenceLayer/repository/RecursoRepository.java
+++ project/src/main/java/com/coworking/project/persistenceLayer/repository/RecursoRepository.java
@@ -2,19 +2,23 @@ package com.coworking.project.persistenceLayer.repository;
 
 import com.coworking.project.persistenceLayer.entity.RecursoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface RecursoRepository extends JpaRepository<RecursoEntity,Long> {
 
-    List<RecursoEntity>  findByRecursoEntity_Id(Long id);
-
     List<RecursoEntity> findByTipoRecurso(String tipoRecurso);
 
-    RecursoEntity findByNombreRecurso(String nombreRecurso);
+    List<RecursoEntity> findByNombreRecursoContainingIgnoreCase(String nombreRecurso);
 
     List<RecursoEntity> findByEstado(String estado);
+
+    @Query("SELECT r FROM RecursoEntity r WHERE :fecha BETWEEN r.fechaInicioTarifa AND r.fechaFinTarifa")
+    List<RecursoEntity> findRecursosConTarifaVigente(@Param("fecha") LocalDate fecha);
 
 }
--- project/src/main/java/com/coworking/project/persistenceLayer/repository/RolRepository.java
+++ project/src/main/java/com/coworking/project/persistenceLayer/repository/RolRepository.java
@@ -2,10 +2,16 @@ package com.coworking.project.persistenceLayer.repository;
 
 import com.coworking.project.persistenceLayer.entity.RolEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 
 public interface RolRepository  extends JpaRepository<RolEntity,Long> {
 
-    RolEntity findByNombreRol(String nombreRol);
+    Optional<RolEntity> findByNombreRol(String nombreRol);
+
+    @Query("SELECT COUNT(u) FROM UsuarioEntity u WHERE u.rolEntity.idRol = :idRol")
+    Long contarUsuariosPorRol(Long idRol);
 
 }
